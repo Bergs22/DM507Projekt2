12a1e08561f7b51d197add30932d7593e9967868
Bergs22 <mads22berggreen@live.dk>
2017-03-22 13:24:48 +0100

    Start

---
+++ src/dm507projekt2/DictBinTree.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dm507projekt2;
+
+/**
+ *
+ * @author Mads
+ */
+public class DictBinTree implements Dict {
+
+    @Override
+    public void insert(int k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int[] orderedTraversal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean search() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
