7608215ca7ed641486677359184170784f2983bb
karim møller <Karimkuku@gmail.com>
2017-03-22 16:03:02 +0100

    addede unit test

---
+++ unitTest/dm507projekt2/DictBinTreeTest.java
@@ -0,0 +1,41 @@
+package dm507projekt2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by karim møller on 22-03-2017.
+ */
+public class DictBinTreeTest {
+    DictBinTree dict;
+
+    @Before
+    public void setUp() throws Exception {
+    dict = new DictBinTree();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void insert() throws Exception {
+    dict.insert(1);
+
+    }
+
+    @Test
+    public void orderedTraversal() throws Exception {
+
+    }
+
+    @Test
+    public void search() throws Exception {
+
+    }
+
+}
